0878418afc03a3124a9bbe14ca629057c6480700
Sumit Bhardwaj <sumit@airbotix.in>
2025-09-26 16:43:34 +0530

    readerhandler build done

--- plugins/datastreamer_plugin/fastdds/ReaderHandler.cpp
+++ plugins/datastreamer_plugin/fastdds/ReaderHandler.cpp
@@ -1,41 +1,11 @@
-// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
-//
-// This file is part of eProsima Fast DDS Visualizer Plugin.
-//
-// eProsima Fast DDS Visualizer Plugin is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// eProsima Fast DDS Visualizer Plugin is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with eProsima Fast DDS Visualizer Plugin. If not, see <https://www.gnu.org/licenses/>.
-
-/**
- * @file ReaderHandler.hpp
- */
-
-// #include <fastdds/dds/domain/DomainParticipantFactory.hpp>
-// #include <fastdds/dds/xtypes/dynamic_types/DynamicDataFactory.hpp>
-// #include <fastdds/dds/xtypes/utils.hpp>
-
 #include "ReaderHandler.hpp"
-#include "utils/utils.hpp"
-#include "utils/dynamic_types_utils.hpp"
+#include <dds/DCPS/XTypes/DynamicDataImpl.h>
+#include <dds/DCPS/XTypes/DynamicDataReaderImpl.h>
+#include <iostream>
 
 namespace eprosima {
 namespace plotjuggler {
 namespace fastdds {
-
-using namespace DDS;
-
-////////////////////////////////////////////////////
-// CREATION & DESTRUCTION
-////////////////////////////////////////////////////
 
 ReaderHandler::ReaderHandler(
         DDS::Topic_var topic,
@@ -43,150 +13,153 @@ ReaderHandler::ReaderHandler(
         DDS::DynamicType_ptr type,
         FastDdsListener* listener,
         const DataTypeConfiguration& data_type_configuration)
-    : topic_(topic)
+    : listener_(listener)
+    , topic_(topic)
     , reader_(datareader)
     , type_(type)
-    , listener_(listener)
+    , data_(nullptr)
     , stop_(false)
     , data_type_configuration_(data_type_configuration)
 {
-    // Create data so it is not required to create it each time and avoid reallocation if possible
-    data_ = DynamicDataFactory::get_instance()->create_data(type_);
-
-    // Set this object as this reader's listener
-    reader_->set_listener(this, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+    create_data_structures_();
 }
 
 ReaderHandler::~ReaderHandler()
 {
-    // Stop the reader
     stop();
-
-    // Delete created data
-    DynamicDataFactory::get_instance()->delete_data(data_);
 }
 
-////////////////////////////////////////////////////
-// INTERACTION METHODS
-////////////////////////////////////////////////////
+ReaderHandler& ReaderHandler::operator=(ReaderHandler&& other)
+{
+    listener_ = other.listener_;
+    topic_ = other.topic_;
+    reader_ = other.reader_;
+    type_ = other.type_;
+    data_ = other.data_;
+    stop_ = other.stop_.load();
+    numeric_data_info_ = std::move(other.numeric_data_info_);
+    string_data_info_ = std::move(other.string_data_info_);
+    data_type_configuration_ = std::move(other.data_type_configuration_);
+    return *this;
+}
+
+ReaderHandler& ReaderHandler::operator=(const ReaderHandler& other)
+{
+    listener_ = other.listener_;
+    topic_ = other.topic_;
+    reader_ = other.reader_;
+    type_ = other.type_;
+    data_ = other.data_;
+    stop_ = other.stop_.load();
+    numeric_data_info_ = other.numeric_data_info_;
+    string_data_info_ = other.string_data_info_;
+    data_type_configuration_ = other.data_type_configuration_;
+    return *this;
+}
 
 void ReaderHandler::stop()
 {
-    // Stop the reader
     stop_ = true;
-    reader_->set_listener(nullptr, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
 }
 
-////////////////////////////////////////////////////
-// LISTENER METHODS [ DATAREADER ]
-////////////////////////////////////////////////////
-
-void ReaderHandler::on_data_available(DDS::DataReader_ptr reader)
+void ReaderHandler::on_data_available(DDS::DataReader* reader)
 {
-  DDS::ReturnCode_t rc = DDS::RETCODE_OK;
-  DDS::SampleInfo info;
+    if (stop_) return;
 
-  // Create a dynamic data instance from a DynamicType_var 'type_' you already have.
-  // Note: DDS::DynamicData_var is a CORBA _var smart pointer.
-  DDS::DynamicData_var data = new DynamicData(type_);
-
-  // Loop until no more data or you decide to stop
-  while (!stop_ && rc == DDS::RETCODE_OK) {
-    // take_next_sample has an overload that accepts DDS::DynamicData_var / pointer
-    rc = reader->take_next_sample(data.inout(), info);
-
-    if (rc == DDS::RETCODE_OK &&
-        info.instance_state == DDS::ALIVE_INSTANCE_STATE &&
-        info.valid_data) {
-
-      // Use 'data' (DDS::DynamicData*) — example: get a member named "speed" if exists
-      try {
-        int32_t speed = 0;
-
-        DDS::ReturnCode_t get_rc = data->get_int32_value(speed, /*member id or name*/ 1);
-        // OR use member name APIs (depends on how you want to access)
-        // Note: many get_*_value methods exist on DDS::DynamicData
-
-      } catch (const CORBA::Exception& ex) {
-        ACE_ERROR((LM_ERROR, "Exception reading dynamic data: %C\n", ex._info().c_str()));
-      }
-
-      // ... process numeric_data_info_, string_data_info_, callbacks etc
+    DDS::DynamicDataReader_var dynamic_reader = DDS::DynamicDataReader::_narrow(reader);
+    if (!dynamic_reader) {
+        std::cerr << "ReaderHandler: Failed to narrow DataReader to DynamicDataReader" << std::endl;
+        return;
     }
-    else if (rc == DDS::RETCODE_NO_DATA) {
-      // no more data available
-      break;
+
+    DDS::DynamicDataSeq data_seq;
+    DDS::SampleInfoSeq info_seq;
+    DDS::ReturnCode_t rc = dynamic_reader->take(
+        data_seq,
+        info_seq,
+        DDS::LENGTH_UNLIMITED,
+        DDS::ANY_SAMPLE_STATE,
+        DDS::ANY_VIEW_STATE,
+        DDS::ANY_INSTANCE_STATE);
+
+    if (rc != DDS::RETCODE_OK) {
+        std::cerr << "ReaderHandler: take failed with code " << rc << std::endl;
+        return;
     }
-  } // while
+
+    for (CORBA::ULong i = 0; i < data_seq.length(); ++i) {
+        if (info_seq[i].valid_data) {
+            nlohmann::json json_data;
+            rc = utils::serialize_data(data_seq[i], json_data);
+            if (rc == DDS::RETCODE_OK) {
+                numeric_data_info_.clear();
+                string_data_info_.clear();
+                utils::get_formatted_data(
+                    topic_name(),
+                    data_type_configuration_,
+                    numeric_data_info_,
+                    string_data_info_,
+                    json_data);
+
+                // Convert SampleInfo timestamp to seconds
+                double timestamp = info_seq[i].source_timestamp.sec +
+                                   info_seq[i].source_timestamp.nanosec / 1e9;
+
+                if (listener_) {
+                    // Call FastDdsListener methods
+                    if (!numeric_data_info_.empty()) {
+                        listener_->on_double_data_read(numeric_data_info_, timestamp);
+                    }
+                    if (!string_data_info_.empty()) {
+                        listener_->on_string_data_read(string_data_info_, timestamp);
+                    }
+                } else {
+                    std::cerr << "ReaderHandler: Listener is null" << std::endl;
+                }
+            } else {
+                std::cerr << "ReaderHandler: Serialization failed for sample " << i << std::endl;
+            }
+        }
+    }
+
+    // Return loaned data
+    dynamic_reader->return_loan(data_seq, info_seq);
 }
-
-
-////////////////////////////////////////////////////
-// VALUES METHODS
-////////////////////////////////////////////////////
 
 std::string ReaderHandler::topic_name() const
 {
-    return topic_->get_name();
+    return topic_ ? topic_->get_name() : "";
 }
 
-const std::string& ReaderHandler::type_name() const
+std::string ReaderHandler::type_name() const
 {
-    return topic_->get_type_name();
+    return type_ ? type_->get_name() : "";
 }
 
-////////////////////////////////////////////////////
-// AUXILIAR METHODS
-////////////////////////////////////////////////////
-
-void ReaderHandler::create_data_structures_(
-        DDS::DynamicData_ptr data /* = nullptr */)
-{
-    // Serialize data to JSON format
-    nlohmann::json serialized_data;
-    if (DDS::RETCODE_OK != utils::serialize_data(data, serialized_data))
-    {
-        DDS_ERROR("READER_HANDLER", "Error serializing data");
-        return;
-    }
-    // Create the structures to store the data introspection information AND the data itself
-    utils::get_formatted_data(
-        topic_name(),
-        data_type_configuration_,
-        numeric_data_info_,
-        string_data_info_,
-        serialized_data);
-
-    DEBUG("Completed type introspection created in topic: " << topic_name() << " with types: ");
-    for (const auto& info : numeric_data_info_)
-    {
-        DEBUG("\tNumeric: " << std::get<0>(info));
-    }
-    for (const auto& info : string_data_info_)
-    {
-        DEBUG("\tString: " << std::get<0>(info));
-    }
-}
-
-////////////////////////////////////////////////////
-// AUXILIAR STATIC METHODS
-////////////////////////////////////////////////////
-
-DDS::StatusMask ReaderHandler::default_listener_mask_()
-{
-    // Only listen for DATA_AVAILABLE (plus participant-related statuses if needed)
-    return DDS::DATA_AVAILABLE_STATUS;
-}
-
-
-std::vector<std::string> ReaderHandler::numeric_data_series_names() const
+std::vector<types::DatumLabel> ReaderHandler::numeric_data_series_names() const
 {
     return utils::get_introspection_type_names(numeric_data_info_);
 }
 
-std::vector<std::string> ReaderHandler::string_data_series_names() const
+std::vector<types::DatumLabel> ReaderHandler::string_data_series_names() const
 {
     return utils::get_introspection_type_names(string_data_info_);
+}
+
+void ReaderHandler::create_data_structures_(DDS::DynamicData_ptr data)
+{
+    if (!data && type_) {
+        data_ = new OpenDDS::XTypes::DynamicDataImpl(type_);
+    } else {
+        data_ = data;
+    }
+    numeric_data_info_.clear();
+    string_data_info_.clear();
+}
+
+DDS::StatusMask ReaderHandler::default_listener_mask_()
+{
+    return DDS::DATA_AVAILABLE_STATUS;
 }
 
 } /* namespace fastdds */
--- plugins/datastreamer_plugin/fastdds/ReaderHandler.hpp
+++ plugins/datastreamer_plugin/fastdds/ReaderHandler.hpp
@@ -1,43 +1,17 @@
-// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
-//
-// This file is part of eProsima Fast DDS Visualizer Plugin.
-//
-// eProsima Fast DDS Visualizer Plugin is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// eProsima Fast DDS Visualizer Plugin is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with eProsima Fast DDS Visualizer Plugin. If not, see <https://www.gnu.org/licenses/>.
-
-/**
- * @file ReaderHandler.hpp
- */
-
 #ifndef _EPROSIMA_PLOTJUGGLERFASTDDSPLUGIN_PLUGINS_DATASTREAMERPLUGIN_FASTDDS_READERHANDLER_HPP_
 #define _EPROSIMA_PLOTJUGGLERFASTDDSPLUGIN_PLUGINS_DATASTREAMERPLUGIN_FASTDDS_READERHANDLER_HPP_
 
 #include <atomic>
-
 #include <nlohmann/json.hpp>
-
 #include <dds/DCPS/DataReaderImpl.h>
 #include <dds/DCPS/TopicImpl.h>
 #include <dds/DCPS/LocalObject.h>
-
 #include <dds/DCPS/Marked_Default_Qos.h>
 #include <dds/DCPS/Service_Participant.h>
 #include <dds/DdsDcpsPublicationC.h>
 #include <dds/DdsDcpsInfrastructureC.h>
-
 #include <dds/DCPS/XTypes/DynamicDataImpl.h>
 #include <dds/DCPS/XTypes/DynamicTypeImpl.h>
-
 #include "FastDdsListener.hpp"
 #include "utils/DataTypeConfiguration.hpp"
 #include "utils/dynamic_types_utils.hpp"
@@ -48,111 +22,44 @@ namespace eprosima {
 namespace plotjuggler {
 namespace fastdds {
 
-/**
- * @brief TODO
- *
- */
 struct ReaderHandler : public OpenDDS::DCPS::LocalObject<DDS::DataReaderListener>
 {
 public:
-
-    ////////////////////////////////////////////////////
-    // CREATION & DESTRUCTION
-    ////////////////////////////////////////////////////
-
-ReaderHandler(
+    ReaderHandler(
         DDS::Topic_var topic,
         DDS::DataReader_var datareader,
         DDS::DynamicType_ptr type,
         FastDdsListener* listener,
         const DataTypeConfiguration& data_type_configuration);
 
-
     virtual ~ReaderHandler();
 
-    ReaderHandler& operator =(
-            ReaderHandler&& other);
-
-    ReaderHandler& operator =(
-            const ReaderHandler& other);
-
-    ////////////////////////////////////////////////////
-    // INTERACTION METHODS
-    ////////////////////////////////////////////////////
+    ReaderHandler& operator=(ReaderHandler&& other);
+    ReaderHandler& operator=(const ReaderHandler& other);
 
     void stop();
 
+    void on_data_available(DDS::DataReader* reader) override;
 
-    ////////////////////////////////////////////////////
-    // LISTENER [ DATAREADER ] METHODS
-    ////////////////////////////////////////////////////
-
-    void on_data_available(
-            DDS::DataReader* reader) override;
-
-
-    ////////////////////////////////////////////////////
-    // VALUES METHODS
-    ////////////////////////////////////////////////////
-
-    const std::string& topic_name() const;
-
-    const std::string& type_name() const;
+    std::string topic_name() const;  // Changed to std::string
+    std::string type_name() const;   // Changed to std::string
 
     std::vector<types::DatumLabel> numeric_data_series_names() const;
-
     std::vector<types::DatumLabel> string_data_series_names() const;
 
-    ////////////////////////////////////////////////////
-    // AUXILIAR METHODS
-    ////////////////////////////////////////////////////
+protected:
+    void create_data_structures_(DDS::DynamicData_ptr data = nullptr);
 
-    void create_data_structures_(
-            DDS::DynamicData_ptr data = nullptr);
-
-    ////////////////////////////////////////////////////
-    // AUXILIAR STATIC METHODS
-    ////////////////////////////////////////////////////
-
-    /**
-     * @brief Get default mask
-     *
-     * Callbacks accepted by this mask:
-     * - on_data_available
-     *
-     * @return DDS::StatusMask with callbacks needed
-     */
     static DDS::StatusMask default_listener_mask_();
 
-
-    ////////////////////////////////////////////////////
-    // INTERNAL VARIABLES
-    ////////////////////////////////////////////////////
-
     FastDdsListener* listener_;
-
-
-    ////////////////////////////////////////////////////
-    // FAST DDS POINTERS
-    ////////////////////////////////////////////////////
-
-    //! Topic related with this DataReader
     DDS::Topic_var topic_;
-
-    //! DataReader
     DDS::DataReader_var reader_;
-
-    //! Type Informantion
     DDS::DynamicType_ptr type_;
-
-    //! Data Type element
     DDS::DynamicData_ptr data_;
-
     std::atomic<bool> stop_;
-
     utils::TypeIntrospectionNumericStruct numeric_data_info_;
     utils::TypeIntrospectionStringStruct string_data_info_;
-
     DataTypeConfiguration data_type_configuration_;
 };
 
